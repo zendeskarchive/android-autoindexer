ae401d92862c9278f7e389238e496cdfbcc57f78
Jerzy Chalupski <jerzy@futuresimple.com>
2013-10-08 11:29:42 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,59 @@
+AutoIndexer
+===========
+Android library for generating db indexes.
+
+Basic usage
+-----------
+Define the relationships between your data models using [Thneed](https://github.com/chalup/thneed) and use this information to generate the proper indexes:
+```java
+for (SqliteIndex index : AutoIndexer.generateIndexes(MODEL_GRAPH)) {
+  db.execSQL(AutoIndexer.getCreateStatement(index));
+}
+```
+
+Note that this will generate the indexes for both ends of the relationships, which might not be exactly what you want. For example it will generate the index for primary keys referenced from other columns. We provide the `Predicate` factory to filter the generation results:
+```java
+FluentIterable<SQLiteIndex> indexes = FluentIterable
+    .from(AutoIndexer.generateIndexes(MODEL_GRAPH))
+    .filter(Predicates.not(isIndexOnColumn(ModelColumns.ID)))
+    .filter(Predicates.not(isIndexOnColumn(BaseColumns._ID)));
+```
+
+And if you don't want index generation, you can still use our API to get the create index statement (although, I admit, it's not a killer feature):
+```java
+AutoIndexer.getCreateStatement(new SQLiteIndex("my_table", "foobar_id"));
+```
+
+Hint: when you're automagically generating the indexes, you want to automagically clean them up as well. Use [SQLiteMaster](https://github.com/futuresimple/sqlitemaster) utility to do this:
+```java
+SQLiteMaster.dropIndexes(db);
+```
+
+Building
+--------
+This is standard maven project. To build it just execute:
+```shell
+mvn clean package
+```
+in directory with pom.xml.
+
+Todo
+----
+* Documentation
+* Unit tests
+
+License
+-------
+    Copyright (C) 2013 Jerzy Chalupski
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License. 
---
+++ src/main/java/com/getbase/autoindexer/AutoIndexer.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2013 Jerzy Chalupski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.getbase.autoindexer;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Sets;
+
+import org.chalup.thneed.ManyToManyRelationship;
+import org.chalup.thneed.ModelGraph;
+import org.chalup.thneed.OneToManyRelationship;
+import org.chalup.thneed.OneToOneRelationship;
+import org.chalup.thneed.PolymorphicRelationship;
+import org.chalup.thneed.RecursiveModelRelationship;
+import org.chalup.thneed.RelationshipVisitor;
+
+import java.util.Set;
+
+public final class AutoIndexer {
+  private AutoIndexer() {
+  }
+
+  private static final Joiner COLUMN_JOINER = Joiner.on(",");
+  private static final Joiner INDEX_NAME_JOINER = Joiner.on("_");
+
+  public static String getCreateStatement(SqliteIndex index) {
+    String indexName = INDEX_NAME_JOINER.join("auto_index", index.mTable, index.mColumns);
+    return "CREATE INDEX " + indexName + " ON " + index.mTable + "(" + COLUMN_JOINER.join(index.mColumns) + ")";
+  }
+
+  public static Predicate<SqliteIndex> isIndexOnColumn(final String columnName) {
+    return new Predicate<SqliteIndex>() {
+      @Override
+      public boolean apply(SqliteIndex index) {
+        return index.mColumns.length == 1 && index.mColumns[0].equalsIgnoreCase(columnName);
+      }
+    };
+  }
+
+  public static Set<SqliteIndex> generateIndexes(ModelGraph<? extends DbTableModel> modelGraph) {
+    final Set<SqliteIndex> indexes = Sets.newHashSet();
+
+    modelGraph.accept(new RelationshipVisitor<DbTableModel>() {
+      @Override
+      public void visit(OneToManyRelationship<? extends DbTableModel> relationship) {
+        DbTableModel model = relationship.mModel;
+        DbTableModel referencedModel = relationship.mReferencedModel;
+
+        indexes.add(new SqliteIndex(model.getDbTable(), relationship.mLinkedByColumn));
+        indexes.add(new SqliteIndex(referencedModel.getDbTable(), relationship.mReferencedModelIdColumn));
+      }
+
+      @Override
+      public void visit(OneToOneRelationship<? extends DbTableModel> relationship) {
+        DbTableModel model = relationship.mModel;
+        DbTableModel linkedModel = relationship.mLinkedModel;
+
+        indexes.add(new SqliteIndex(linkedModel.getDbTable(), relationship.mLinkedByColumn));
+        indexes.add(new SqliteIndex(model.getDbTable(), relationship.mParentModelIdColumn));
+      }
+
+      @Override
+      public void visit(RecursiveModelRelationship<? extends DbTableModel> relationship) {
+        DbTableModel model = relationship.mModel;
+
+        indexes.add(new SqliteIndex(model.getDbTable(), relationship.mModelIdColumn));
+        indexes.add(new SqliteIndex(model.getDbTable(), relationship.mGroupByColumn));
+      }
+
+      @Override
+      public void visit(ManyToManyRelationship<? extends DbTableModel> relationship) {
+        // no implementation necessary
+      }
+
+      @Override
+      public void visit(PolymorphicRelationship<? extends DbTableModel> relationship) {
+        DbTableModel model = relationship.mModel;
+        indexes.add(new SqliteIndex(model.getDbTable(), relationship.mTypeColumnName, relationship.mIdColumnName));
+
+        for (DbTableModel dbTableModel : relationship.mPolymorphicModels.values()) {
+          indexes.add(new SqliteIndex(dbTableModel.getDbTable(), relationship.mPolymorphicModelIdColumn));
+        }
+      }
+    });
+
+    return indexes;
+  }
+}
